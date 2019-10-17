78e04fb04cee02cc2effcbad41e0d2b62a207452
AFelipe <afelipelc@gmail.com>
2019-10-17 10:27:56 -0500

    cors eneabled

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use(cors());
 app.use('/', routes());
 
 app.listen(5000);
